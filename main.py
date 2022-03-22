ad325324057b1785e6f7629d44d322350887ce0b
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2022-03-22 16:25:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import asyncio
+import datetime
+import json
+import os
+import time
+import discord
+import discord_slash
+from discord.ext import commands, tasks
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.model import ButtonStyle
+from discord_slash.utils import manage_components
+
+with open('./config.json', 'r') as cjson:
+    config = json.load(cjson)
+
+guilds = []
+
+
+def get_prefix(client, message):
+    prefixes = [config["prefix"]]
+
+    if not message.guild:
+        prefixes = [config["prefix"]]
+
+    return commands.when_mentioned_or(*prefixes)(client, message)
+
+
+bot = commands.Bot(command_prefix=get_prefix, description="giveaway bot",
+                   case_insensitive=True)
+slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    while len(guilds) < 1:
+        async for guild in bot.fetch_guilds(limit=150):
+            guilds.append(guild.name)
+    print(guilds)
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f'cogs.{filename}')
+
+bot.run(config["token"], bot=True, reconnect=True)
